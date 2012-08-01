db78a1721e57d7ad8af2c88acf72b7fb8eda7ef3
Yawar Amin <yawar.amin@gmail.com>
2012-07-31 23:14:44 -0400

    End of chapter 3

---
+++ Dollar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TDDMoney {
+  class Dollar {
+    public Dollar(int amount) {
+      Amount = amount;
+    }
+
+    public Dollar Times(int multiplier) {
+      return new Dollar(Amount * multiplier);
+    }
+
+    public override bool Equals(object o) {
+      return Amount == ((Dollar)o).Amount;
+    }
+
+    public int Amount { get; set; }
+
+    static void Main() {
+    }
+  }
+}
---
+++ Tests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace TDDMoney {
+  [TestFixture]
+  class With_a_Dollar {
+    [Test]
+    public void Test_multiplication() {
+      Dollar five = new Dollar(5);
+      Dollar product = five.Times(2);
+      
+      Assert.AreEqual(10, product.Amount);
+      product = five.Times(3);
+      Assert.AreEqual(15, product.Amount);
+    }
+
+    [Test]
+    public void Test_equality() {
+      Assert.IsTrue(new Dollar(5).Equals(new Dollar(5)));
+      Assert.IsFalse(new Dollar(5).Equals(new Dollar(6)));
+    }
+  }
+}
